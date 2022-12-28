4fc40a73ce7e500616dba5399a874f99f32ef42b
ShingoYadomoto <shingo.yadomoto@gmail.com>
2022-12-28 17:18:58 +0900

    set up json server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/ShingoYadomoto/nanikiru/backend/data"
 )
 
+func handler(rw http.ResponseWriter, _ *http.Request) {
+	d := data.GetBeautifier().Beautify()
+	j, err := json.Marshal(d)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		log.Printf("failed to marshal to json. err: %s", err.Error())
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	_, err = rw.Write(j)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		log.Printf("failed to write resuponse. err: %s", err.Error())
+	}
+}
+
 func main() {
-	data.MustSetup()
+	http.HandleFunc("/nanikiru", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- backend/data/db.go
+++ backend/data/db.go
@@ -1,50 +1,33 @@
 package data
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const dbFile = "nanikiru.db"
-
-type DB struct {
-	*sql.DB
+type Beautifier struct {
+	data []Nanikiru
 }
 
-func NewDB() *DB {
+func newBeautifier() *Beautifier {
+	return &Beautifier{data: data}
+}
+
+var nanikiruBeautifier *Beautifier
+
+func init() {
+	nanikiruBeautifier = newBeautifier()
+}
+
+func GetBeautifier() *Beautifier {
+	return nanikiruBeautifier
 }
 
 type Nanikiru struct {
-	ID     string `db:"id"`
 	Hands  string `db:"hands"`
 	Answer string `db:"answer"`
 	Page   int    `db:"page"`
 }
 
-func (db *DB) QueryNanikiruList() ([]Nanikiru, error) {
-	rows, err := db.db.Query(fmt.Sprintf("SELECT * from %s", NanikiruTable))
-	if err != nil {
-		return nil, err
-	}
-
-	ret := []Nanikiru{}
-	defer rows.Close()
-	for rows.Next() {
-		r := Nanikiru{}
-
-		err = rows.Scan(&r.ID, &r.Hands, &r.Answer)
-		if err != nil {
-			return nil, err
-		}
-
-		ret = append(ret, r)
-	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-
-	return ret, nil
+func (b *Beautifier) Beautify() []Nanikiru {
+	return b.data
 }
