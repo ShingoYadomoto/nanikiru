5456f04f8ea467ada9822a0eac32f838b6179ff4
ShingoYadomoto <shingo.yadomoto@gmail.com>
2022-12-29 17:48:36 +0900

    get random question server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,27 +4,78 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/ShingoYadomoto/nanikiru/backend/data"
 )
 
-func handler(rw http.ResponseWriter, _ *http.Request) {
-	d := data.GetBeautifier().Beautify()
-	j, err := json.Marshal(d)
+const excludeIDQueryKey = "exclude_id"
+
+func getRandomQuestionHandler(rw http.ResponseWriter, r *http.Request) {
+	ret, err := func() ([]byte, error) {
+		var (
+			query            = r.URL.Query()
+			excludeIDCSV     = query.Get(excludeIDQueryKey)
+			excludeIDStrList = strings.Split(excludeIDCSV, ",")
+			excludeIDList    = make([]data.QuestionID, 0, len(excludeIDStrList))
+		)
+
+		for _, excludeIDStr := range excludeIDStrList {
+			if excludeIDStr == "" {
+				continue
+			}
+
+			id, err := data.NewQuestionIDFromStr(excludeIDStr)
+			if err != nil {
+				return nil, err
+			}
+
+			excludeIDList = append(excludeIDList, id)
+		}
+
+		d, err := data.GetQuestioner().GetQuestion(excludeIDList)
+		if err != nil {
+			return nil, err
+		}
+
+		j, err := json.Marshal(d)
+		if err != nil {
+			return nil, err
+		}
+
+		return j, nil
+	}()
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
-		log.Printf("failed to marshal to json. err: %s", err.Error())
+		log.Printf("failed to get byte data. err: %s", err.Error())
 	}
 
 	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(j)
+	_, err = rw.Write(ret)
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
 		log.Printf("failed to write resuponse. err: %s", err.Error())
 	}
 }
 
+// for development
+func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Content-Type", "application/json")
+
+		// preflight用に200でいったん返す
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/nanikiru", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/questions", CORSMiddleware(getRandomQuestionHandler))
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
--- backend/data/db.go
+++ backend/data/db.go
@@ -1,33 +1,81 @@
 package data
 
 import (
+	"errors"
+	"math/rand"
+	"strconv"
+	"time"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type Beautifier struct {
-	data []Nanikiru
+type QuestionID uint
+
+func NewQuestionIDFromStr(s string) (QuestionID, error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return QuestionID(i), nil
 }
 
-func newBeautifier() *Beautifier {
-	return &Beautifier{data: data}
+type Questioner struct {
+	data map[QuestionID]Nanikiru
 }
 
-var nanikiruBeautifier *Beautifier
+func newQuestioner() *Questioner {
+	dl := newData()
+
+	m := map[QuestionID]Nanikiru{}
+	for i, d := range dl {
+		m[QuestionID(i+1)] = d
+	}
+	return &Questioner{data: m}
+}
+
+var nanikiruQuestioner *Questioner
 
 func init() {
-	nanikiruBeautifier = newBeautifier()
+	nanikiruQuestioner = newQuestioner()
+	rand.Seed(time.Now().UnixNano())
 }
 
-func GetBeautifier() *Beautifier {
-	return nanikiruBeautifier
+func GetQuestioner() *Questioner {
+	return nanikiruQuestioner
 }
 
 type Nanikiru struct {
-	Hands  string `db:"hands"`
-	Answer string `db:"answer"`
-	Page   int    `db:"page"`
+	ID     uint
+	Hands  string
+	Answer string
+	Page   int
 }
 
-func (b *Beautifier) Beautify() []Nanikiru {
-	return b.data
+func (q *Questioner) GetQuestion(excludeIDList []QuestionID) (*Nanikiru, error) {
+	var (
+		max          = len(q.data)
+		maxfailCount = max - len(excludeIDList)
+		excludeIDMap = map[QuestionID]struct{}{}
+	)
+
+	for _, excludeID := range excludeIDList {
+		excludeIDMap[excludeID] = struct{}{}
+	}
+
+	for {
+		if maxfailCount == 0 {
+			return nil, errors.New("over max fail count")
+		}
+
+		randomID := QuestionID(rand.Intn(max + 1))
+		if _, ng := excludeIDMap[randomID]; ng {
+			maxfailCount--
+			continue
+		}
+
+		ret := q.data[randomID]
+
+		return &ret, nil
+	}
 }
