30213e7e724237273d31b458ceab67028c2599c9
ShingoYadomoto <shingo.yadomoto@gmail.com>
2022-12-31 11:29:47 +0900

    remove get mock data

--- frontend/src/data/questioner/questioner.ts
+++ frontend/src/data/questioner/questioner.ts
@@ -1,15 +1,14 @@
 import data from "../infrastructure/data";
-import {IAnswerRequest, IQuestionData} from "../infrastructure/schema";
+import {IAnswerRequest} from "../infrastructure/schema";
 import {PaiDetail, PaiType} from "../../component/pai/pai";
 import {HandDetail} from "../../component/hand/hand";
 import {AnswerDetail} from "../../component/answer/answer";
 import {QuestionID} from "../../component/question/question";
 
 const getNextQuestion = (excludeID: QuestionID[]): [HandDetail, QuestionID] => {
-    // ToDo: mapping data to component schema
+    let id: QuestionID = 0
+    let paiList: PaiDetail[] = []
 
-    let id: QuestionID
-    let paiList: PaiDetail[]
     data.getQuestion(excludeID)
         .then((response) => {
             id = response.data.id;
@@ -19,23 +18,6 @@ const getNextQuestion = (excludeID: QuestionID[]): [HandDetail, QuestionID] => {
             console.log(e);
         });
 
-    paiList = [
-        {type: PaiType.Manzu, index: 5, isFolou: false, isBonus: false,},
-        {type: PaiType.Manzu, index: 6, isFolou: false, isBonus: false,},
-        {type: PaiType.Pinzu, index: 5, isFolou: false, isBonus: false,},
-        {type: PaiType.Pinzu, index: 6, isFolou: false, isBonus: false,},
-        {type: PaiType.Pinzu, index: 8, isFolou: false, isBonus: false,},
-        {type: PaiType.Pinzu, index: 9, isFolou: false, isBonus: false,},
-        {type: PaiType.Sozu, index: 4, isFolou: false, isBonus: false,},
-        {type: PaiType.Sozu, index: 4, isFolou: false, isBonus: false,},
-        {type: PaiType.Sozu, index: 6, isFolou: false, isBonus: false,},
-        {type: PaiType.Sozu, index: 6, isFolou: false, isBonus: false,},
-        {type: PaiType.Sozu, index: 7, isFolou: false, isBonus: false,},
-        {type: PaiType.Zi, index: 7, isFolou: false, isBonus: false,},
-        {type: PaiType.Zi, index: 7, isFolou: false, isBonus: false,},
-        {type: PaiType.Zi, index: 7, isFolou: false, isBonus: false,},
-    ]
-    id = 1
 
     const hand: HandDetail = {
         paiList: paiList
@@ -49,24 +31,24 @@ const answer = (id: QuestionID, userAnswer: PaiDetail) => {
         userAnswer: userAnswer,
     }
 
-    let isCorrect: boolean
-    let correctAnswer: PaiDetail[]
-    // ToDo: mapping data to component schema
-    const resultDTO = data.postAnswer(id, body)
+    let isCorrect: boolean = false
+    let correctAnswer: PaiDetail[] = []
+    data.postAnswer(id, body)
         .then((response) => {
             isCorrect = response.data.isCorrect;
             correctAnswer = response.data.correctAnswer;
         })
         .catch((e: Error) => {
+            // ToDO: remove mock data
+            isCorrect = false
+            correctAnswer = [
+                {type: PaiType.Pinzu, index: 8, isFolou: false, isBonus: false,},
+                {type: PaiType.Pinzu, index: 9, isFolou: false, isBonus: false,},
+            ]
+
             console.log(e);
         });
 
-
-    isCorrect = false
-    correctAnswer = [
-        {type: PaiType.Pinzu, index: 8, isFolou: false, isBonus: false,},
-        {type: PaiType.Pinzu, index: 9, isFolou: false, isBonus: false,},
-    ]
 
     const incorrectResult: AnswerDetail = {
         isCorrect: isCorrect,
